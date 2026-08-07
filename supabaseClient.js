35fbb32ce36d032e4086693f2d7530df0f7a6b64
mendozaalan2211 <mendozaalan879@gmail.com>
2026-08-07 11:55:39 -0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -7,8 +7,8 @@ import { createClient } from "@supabase/supabase-js";
 //  Settings (engrane) -> API
 // ============================================================
 
-const SUPABASE_URL = "PEGA_AQUI_TU_PROJECT_URL";
-const SUPABASE_ANON_KEY = "PEGA_AQUI_TU_ANON_KEY";
+const SUPABASE_URL = "https://yjhessebmbiorkkbdeag.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqaGVzc2VibWJpb3Jra2JkZWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMTIwODUsImV4cCI6MjEwMTY4ODA4NX0.7epxFjG5MILaF-oY2EYfbCwtNQ4cUEUQbQinhUs_af4";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
